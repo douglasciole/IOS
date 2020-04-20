24b5a95ac2c126cabfedd66f8d1e6b3fd5f7da1d
Douglas Ciole <douglasciole@gmail.com>
2020-04-19 17:39:17 -0700

    Assignment 2 done

---
+++ ASS 2/ASS 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ASS 2
+//
+//  Created by Douglas Gois on 2020-04-17.
+//  Copyright © 2020 Douglas Gois. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
